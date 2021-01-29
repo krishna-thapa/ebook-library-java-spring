ed83671e38dfc0f75a862e4061a37a024a50d101
krishna <krishna.nature91@gmail.com>
2021-01-29 23:39:17 +0000

    New files for web security are added and controller

--- src/main/java/com/krishna/onlinelibrary/repository/AppUserRepository.java
+++ src/main/java/com/krishna/onlinelibrary/repository/AppUserRepository.java
@@ -2,6 +2,7 @@ package com.krishna.onlinelibrary.repository;
 
 
 import com.krishna.onlinelibrary.models.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,7 +10,7 @@ import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     /*
         Find the user by email address
--- src/main/java/com/krishna/onlinelibrary/service/RegistrationService.java
+++ src/main/java/com/krishna/onlinelibrary/service/RegistrationService.java
@@ -1,7 +1,9 @@
 package com.krishna.onlinelibrary.service;
 
 import com.krishna.onlinelibrary.request.RegistrationRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return "Works";
