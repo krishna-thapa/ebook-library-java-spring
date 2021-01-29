c15748203d1510f56ee48a677e36eff2ff300df5
krishna <krishna.nature91@gmail.com>
2021-01-29 23:02:47 +0000

    Added new app user service and repository

---
+++ src/main/java/com/krishna/onlinelibrary/repository/AppUserRepository.java
@@ -0,0 +1,18 @@
+package com.krishna.onlinelibrary.repository;
+
+
+import com.krishna.onlinelibrary.appUser.AppUser;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+
+    /*
+        Find the user by email address
+     */
+    Optional<AppUser> findByEmail(String email);
+}
