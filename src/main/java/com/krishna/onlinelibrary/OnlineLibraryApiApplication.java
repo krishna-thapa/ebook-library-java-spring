2872a85054119d9e87e5c7e356f270d3533a4a13
krishna <krishna.nature91@gmail.com>
2021-01-29 22:17:58 +0000

    Initial commit

---
+++ src/main/java/com/krishna/onlinelibrary/OnlineLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.onlinelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLibraryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krishna/onlinelibrary/OnlineLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishna.onlinelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
